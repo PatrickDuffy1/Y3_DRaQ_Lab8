a8596f4c4db3f2227d403825ec169c0c52537f62
Patrick Duffy <G00408635@atu.ie>
2023-11-24 15:19:43 +0000

    first commit

---
+++ src/components/books.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import BooksItem from "./bookItem";
+
+function Books(props)
+{
+    // Creates map out of myBooks
+    return props.myBooks.map
+    (
+        (book)=>
+        {
+            return <BooksItem myBook={book} key={book._id}></BooksItem> // Passes the current book to BooksItem and sets the _id as the key
+        }
+    );
+}
+
+export default Books;
+    
